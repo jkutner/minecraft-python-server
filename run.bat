c868b3c46812c6dbf0f5298d7974efd4c809b49a
GK <jpkutner+github@gmail.com>
2020-12-31 15:07:50 -0600

    add custom config directory

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 FOR /F "tokens=*" %%i in ('type .env') do SET %%i
-docker run -it --env-file .env -p 4711:4711 -p 8080:8080 -p 25566:25566 jkutner/pycraft
+docker run -it --env-file .env -v %~dp0/config:/workspace/config:ro -p 4711:4711 -p 8080:8080 -p 25566:25566 jkutner/pycraft
