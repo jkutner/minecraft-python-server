882a63d9b13398b48f0da5d8ebb896a30ab2ba0a
GK <jpkutner+github@gmail.com>
2020-12-31 16:44:16 -0600

    save the world

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 FOR /F "tokens=*" %%i in ('type .env') do SET %%i
-docker run -it --env-file .env -v %~dp0/config:/workspace/config:ro -p 4711:4711 -p 8080:8080 -p 25566:25566 jkutner/pycraft
+if not exist "%~dp0/world" mkdir %~dp0/world
+docker run -it --env-file .env -v %~dp0/config:/workspace/config:ro -v %~dp0/world:/workspace/world -p 4711:4711 -p 8080:8080 -p 25566:25566 jkutner/pycraft
