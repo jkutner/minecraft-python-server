4e07fdd1402a737919e728f4abe88a7d1c9c0e42
Joe Kutner <jpkutner@gmail.com>
2020-12-31 17:17:59 -0600

    Improve run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
-FOR /F "tokens=*" %%i in ('type .env') do SET %%i
-if not exist "%~dp0/world" mkdir %~dp0/world
-docker run -it --env-file .env -v %~dp0/config:/workspace/config:ro -v %~dp0/world:/workspace/world -p 4711:4711 -p 8080:8080 -p 25566:25566 jkutner/pycraft
+if not exist "world" mkdir world
+if not exist ".env" echo. 2>.env
+docker run -it^
+    --env-file .env^
+    -v %~dp0/config:/workspace/config:ro^
+    -v %~dp0/world:/workspace/world^
+    -p 4711:4711 -p 8080:8080 -p 25566:25566^
+    jkutner/pycraft
